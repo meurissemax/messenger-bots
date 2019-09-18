dd385217216af20cd817a50197bfe21d9ec92f0b
Maxime Meurisse <max.meurisse@outlook.com>
2019-09-18 21:19:33 +0200

    ✨ Add new bots

---
+++ automatic_messages/wikipedia_search.py
@@ -0,0 +1,72 @@
+# -*- coding: UTF-8 -*-
+
+#######################
+# Importing libraries #
+#######################
+
+from fbchat import Client
+from fbchat.models import *
+
+import wikipedia
+
+################
+# General data #
+################
+
+KEYWORD_MESSAGE = "@wikipedia"
+
+WIKIPEDIA_LANG = "fr"
+
+##############################
+# Defining the Messenger bot #
+##############################
+
+class MessengerBot(Client):
+
+    # Defining constructor
+
+    def __init__(self, username, password, thread_type, thread_id):
+        self.THREAD_TYPE = thread_type
+        self.THREAD_ID = thread_id
+
+        Client.__init__(self, username, password, "None", 1)
+
+    # Defining the 'action' method
+
+    def action(self):
+        self.listen()
+
+    # Overwrite the 'onMessage' method
+
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+
+        # We check if we are in the correct conversation
+
+        if thread_type == self.THREAD_TYPE and thread_id == self.THREAD_ID:
+
+            # We check the receive message
+
+            message = message_object.text
+
+            if message.startswith(KEYWORD_MESSAGE):
+                query = message[len(KEYWORD_MESSAGE) + 1:len(message)]
+
+                wikipedia.set_lang(WIKIPEDIA_LANG)
+
+                try:
+                    result = wikipedia.summary(query, sentences=1)
+                except DisambiguationError as de:
+                    result = str(de)
+                except PageError as pe:
+                    result = str(pe)
+                except:
+                    result = "Unknow error"
+
+                # We send the message
+
+                try:
+                    self.send(Message(text=result), thread_id=thread_id, thread_type=thread_type)
+                except FBchatException:
+                    print("Request failed (is the ID ou thread type correct ?)")
+        else:
+            super(MessengerBot, self).onMessage(author_id=author_id, message_object=message_object, thread_id=thread_id, thread_type=thread_type, **kwargs)
